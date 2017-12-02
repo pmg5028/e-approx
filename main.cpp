61b4557781b12ff1a2b24f6d6d9b94cbee1b53f3
pmg5028 <34195398+pmg5028@users.noreply.github.com>
2017-12-02 18:33:45 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,21 +16,19 @@ int main(int argc, const char * argv[]) {
     srand (static_cast <unsigned> (time(0)));
     float iterations;
     int d = 0;
-    
     cout << "How many iterations? ";
     cin >> iterations;
 
-    for( int c = 0; c <= iterations; c = c + 1){
+    for( int c = 1; c <= iterations; c = c + 1){
         float a = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
         float b = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-        
-        for( int c = 2 ;a + b < 1; c = c + 1) {
+        int s = 2;
+        for( int j = 2 ;a + b < 1; j = j + 1) {
             a = a + b;
             b = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-            d = d + c;
-            
+            s = j + 1;
         }
-        
+        d = d + s;
         
     }
     cout << d/iterations ;
